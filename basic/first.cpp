54c728ccfb568a4aa5036e1b27bc0bad6aa7fab8
Aaryan <aaryanofficial64@gmail.com>
2025-01-15 08:36:31 +0530

    First program

---
+++ basic/first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;// do this
+int main()
+{
+    cout<<"i am\n";// only for this
+     std :: cout <<"aaryan" << std :: endl;// this doesn't need it
+     std :: cout <<"hello !\n";// this doesn't need it
+     printf("how are you?\n"); // using c 
+    return 0;
+}
