df8b481f6f221be652038bd891221d69c41bb801
ryan-4u <aaryanofficial64@gmail.com>
2025-03-20 23:03:25 +0530

    data types and sizeof

---
+++ C++ with DSA/Basics of Programming/Variable and Data types/datatype.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+    int a = 10;
+    float b = 12.45 ;
+    double c = 1.9999679 ;
+    char d = 'a' ;
+    bool e = true ;
+    cout<<"int a :"<<a<<endl;
+    cout<<"float b :"<<b<<endl;
+    cout<<"double c :"<<c<<endl;
+    cout<<"char d :"<<d<<endl;
+    cout<<"bool e :"<<e<<endl;
+    cout<< sizeof(int) <<endl ;
+     cout<< sizeof(float) <<endl ;
+      cout<< sizeof(double) <<endl ;
+       cout<< sizeof(char) <<endl ;
+        cout<< sizeof(bool) <<endl ;
+    return 0 ;
+}
