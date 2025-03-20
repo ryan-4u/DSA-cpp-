2090925826f9d081e3b65aa5cb736a993069efb2
ryan-4u <aaryanofficial64@gmail.com>
2025-03-20 22:56:21 +0530

    newline

---
+++ C++ with DSA/Basics of Programming/Variable and Data types/newline.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+    // '<<endl' or '\n' can be use d for newline
+    cout<<"Hey bro !"<<endl<<"how are you\n";
+    cout<<"Pattern:\n";
+    cout<<"****\n***\n**\n*\n";
+    return 0 ;
+}
