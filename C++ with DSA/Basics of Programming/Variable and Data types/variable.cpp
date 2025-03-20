a2303956b307aa9e5a59e9284bfce3d11db45315
ryan-4u <aaryanofficial64@gmail.com>
2025-03-20 22:58:43 +0530

    variable

---
+++ C++ with DSA/Basics of Programming/Variable and Data types/variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+    int a = 10 ;
+    int b = 5 ;
+    cout<<"a is equal to :"<< a <<endl ;
+    cout<<"b is equal to :"<< b <<endl ;
+    return 0 ;
+}
