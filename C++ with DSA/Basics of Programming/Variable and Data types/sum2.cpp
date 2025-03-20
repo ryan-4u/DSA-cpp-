7dc4d18184c1247d2a9208479d33a160399a8b17
ryan-4u <aaryanofficial64@gmail.com>
2025-03-20 23:15:06 +0530

    sum where variable are input

---
+++ C++ with DSA/Basics of Programming/Variable and Data types/sum2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ;
+
+int main() {
+    int a;
+    cout<<"enter a : " ;
+    cin>> a ;
+    int b;
+    cout<<"enter b : " ;
+    cin>> b ;
+    int sum = a+b ;
+    cout<<"The sum is "<<sum<<endl ;
+    return 0 ;
+}
