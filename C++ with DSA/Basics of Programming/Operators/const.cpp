5c21f8ce320c0e5a052c57c4571de3ecdc5f6dc3
ryan-4u <aaryanofficial64@gmail.com>
2025-03-22 22:51:08 +0530

    constant and macros

---
+++ C++ with DSA/Basics of Programming/Operators/const.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define x 25 ;
+using namespace std ;
+
+int main()
+{
+    const float PI = 3.1459 ;
+    cout<< x << endl ;
+    cout<< PI << endl ;
+    //PI= 3.14 ;
+    cout<<PI<<endl ;
+    return 0;
+}
