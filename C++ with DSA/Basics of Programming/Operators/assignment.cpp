a2c82d85404cfffb4be1ac6d40af175569e73ec4
ryan-4u <aaryanofficial64@gmail.com>
2025-03-23 09:54:36 +0530

    assignment and relational

--- C++ with DSA/Basics of Programming/Operators/assignment.cpp
+++ C++ with DSA/Basics of Programming/Operators/assignment.cpp
@@ -3,6 +3,17 @@ using namespace std ;
 
 int main()
 {
-    
+    int a = 5 ;
+    // '='
+    int b = a ;
+    cout<< (b) <<endl ;
+    // '+='
+    cout<< (b+=5) <<endl ;
+    // '-='
+    cout<< (b-=5) <<endl ;
+    // '*='
+    cout<< (b*=5) <<endl ;
+    // '/='
+    cout<< (b/=5) <<endl ;
     return 0;
 }
--- C++ with DSA/Basics of Programming/Operators/relational.cpp
+++ C++ with DSA/Basics of Programming/Operators/relational.cpp
@@ -3,6 +3,13 @@ using namespace std ;
 
 int main()
 {
-    
+    int a = 5 ;
+    int b = 2 ;
+    cout<< (a<b) << endl ; //0
+    cout<< (a>b) << endl ; //1
+    cout<< (a<=b) << endl ; //0
+    cout<< (a>=b) << endl ; //1
+    cout<< (a==b) << endl ; //0
+    cout<< (a!=b) << endl ; //1
     return 0;
 }
