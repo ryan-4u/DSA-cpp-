1876b96ac0ef76791670946753e6d29e3a250463
ryan-4u <aaryanofficial64@gmail.com>
2025-03-23 09:34:58 +0530

    arithmetic

--- C++ with DSA/Basics of Programming/Operators/arithmetic.cpp
+++ C++ with DSA/Basics of Programming/Operators/arithmetic.cpp
@@ -3,6 +3,12 @@ using namespace std ;
 
 int main()
 {
-    
+    int a = 5 ;
+    int b = 2 ;
+    cout<< (a+b) << endl ;
+    cout<< (a-b) << endl ;
+    cout<< (a*b) << endl ;
+    cout<< (a/b) << endl ;
+    cout<< (a%b) << endl ;
     return 0;
 }
