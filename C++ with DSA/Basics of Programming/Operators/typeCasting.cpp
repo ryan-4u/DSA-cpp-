91d9b2a914cea5c7bab8c10c4828b3167dd1968f
ryan-4u <aaryanofficial64@gmail.com>
2025-03-23 09:30:22 +0530

    type casting

--- C++ with DSA/Basics of Programming/Operators/typeCasting.cpp
+++ C++ with DSA/Basics of Programming/Operators/typeCasting.cpp
@@ -3,6 +3,18 @@ using namespace std ;
 
 int main()
 {
-    
+    // implicit conversion
+    cout<< (10/3) << endl ;
+    cout<< (10/3.0) << endl ;
+    cout<< ('A'+1) <<endl ;
+    // explicit conversion
+    cout<< (int)('a') << endl ;
+    int PI = 3.14 ;
+    cout<< (int)(PI) << endl ;
+    cout<< ((float)10/3) << endl ;
+    cout<< (char)('A'+1) << endl ;
+    //practice question
+    cout<< (bool)3 + 2 << endl ;
+    cout<< (23.5 + 2 + 'A') << endl ;
     return 0;
 }
