f05cd0bdcaf715d9fdeeb823e22fc3b54b587ff9
ryan-4u <aaryanofficial64@gmail.com>
2025-03-27 12:12:26 +0530

    for loop

---
+++ C++ with DSA/Basics of Programming/Loops/for.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std ;
+int main() {
+    int i ;
+    for( i=1 ; i<=5 ; i++){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+    cout<<"last value of num is : "<<i<<endl;
+    return 0;
+}
---
+++ C++ with DSA/Basics of Programming/Loops/nNaturalNumber.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main() {
+    int n,i ;
+    cout<<"Enter naturnal number N ";
+    cin>>n ;
+    for( i=1 ; i<=n ; i++){
+        cout<<i<<" ";
+    }
+    cout<<"\n";
+ 
+    return 0;
+}
