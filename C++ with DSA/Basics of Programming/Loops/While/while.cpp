52d8b79b7f9a07ca6636f5662a4a8f691eeb5a77
ryan-4u <aaryanofficial64@gmail.com>
2025-03-27 12:28:26 +0530

    while loop

---
+++ C++ with DSA/Basics of Programming/Loops/While/while.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main() {
+    int i=1 ;
+    while( i<=5)
+    {
+        cout<<i<<" ";
+        i++ ;
+    }
+    cout<<"\n";
+     
+    return 0;
+}
