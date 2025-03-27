1e0bfd5f94b6bfc5f5d2c805f80fd44ee22390aa
ryan-4u <aaryanofficial64@gmail.com>
2025-03-27 15:05:31 +0530

    sum of odd digits of a no

---
+++ C++ with DSA/Basics of Programming/Loops/While/sumOfOddDigit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main() {
+    int num ;
+    cout<<"Enter number: ";
+    cin>>num ;
+
+    int sum = 0;
+    while(num>0)
+    {
+        int ld = num%10 ; //ld = last digit
+        if( ld%2 != 0){
+            sum += ld ;
+        }
+        num /= 10 ;
+    }
+    cout<<"sum of digits of the number is "<<sum<<endl;
+    return 0;
+}
