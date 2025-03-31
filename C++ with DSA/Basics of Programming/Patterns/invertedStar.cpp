d432f625ee2baf19844023090f912736f0633363
ryan-4u <aaryanofficial64@gmail.com>
2025-03-31 14:19:32 +0530

    inverted star

--- C++ with DSA/Basics of Programming/Patterns/invertedStar.cpp
+++ C++ with DSA/Basics of Programming/Patterns/invertedStar.cpp
@@ -5,9 +5,9 @@ int main()
     int n;
     cout<<"Enter n : ";
     cin>>n;
-    for(int i=0 ;i<n ; i++)
+    for(int i=n ;i>0 ; i--)
     {
-        for(int j=0 ;j<=i;j++)
+        for(int j=i ;j>0;j--)
         {
             cout<<"* ";
         }
