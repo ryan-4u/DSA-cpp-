de8ca2d2934b2f34e3a808d96f7994636a0c63f1
ryan-4u <aaryanofficial64@gmail.com>
2025-03-31 14:35:31 +0530

    character pyramid

---
+++ C++ with DSA/Basics of Programming/Patterns/characterPyramid.cpp
@@ -0,0 +1,22 @@
+//A
+// B C 
+// D E F
+// G H I J
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    char ch = 'A' ;
+    for(int i=0 ;i<n ; i++)
+    {
+        for(int j=0 ;j<=i;j++)
+        {
+            cout<<ch<<" ";
+            ch++ ;
+        }
+        cout<<endl;
+    }
+}
