c0ed433c5814f56cda4964bb1ddd5fac59d4a381
ryan-4u <aaryanofficial64@gmail.com>
2025-04-01 18:43:33 +0530

    sum of digits assign ques

--- C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/sumOfDigits.cpp
+++ C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/sumOfDigits.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std ;
 //function
-
+int sumOfDigits(int n){
+    int sum = 0 ;
+    while(n>0){
+        int lastDigit = n%10 ;
+        sum += lastDigit ;
+        n /= 10 ;
+    }
+    return sum ;
+}
 //main
 int main(){
-
+    int n;
+    cout<<"Enter Number: ";
+    cin>>n ;
+    cout<<"sum of digits:"<<sumOfDigits(n)<<endl;
     return 0 ;
 }
