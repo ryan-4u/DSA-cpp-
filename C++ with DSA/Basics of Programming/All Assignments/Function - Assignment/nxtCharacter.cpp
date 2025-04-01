1061297581904dd186030993d6351fc832651b21
ryan-4u <aaryanofficial64@gmail.com>
2025-04-01 18:53:08 +0530

    assign ques next character

--- C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/nxtCharacter.cpp
+++ C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/nxtCharacter.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std ;
 //function
-
+char nxtCharacter(char x){
+    char ch = x + 1 ;
+    return ch ;
+}
 //main
 int main(){
-
+    char ch ;
+    cout<<"Enter character: ";
+    cin>>ch;
+    cout<<"Next charcter is "<<nxtCharacter(ch)<<endl;
     return 0 ;
 }
