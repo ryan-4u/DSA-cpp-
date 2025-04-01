1a1ae11ea25e572ea28c28580a54ef97ebd6a5fa
ryan-4u <aaryanofficial64@gmail.com>
2025-04-01 19:05:02 +0530

    palindrome - assign ques

--- C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/palindrome.cpp
+++ C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/palindrome.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std ;
 //function
-
+bool isPalindrome(int num){
+    int origNum = num ;
+    int newNum = 0 ;
+    while(num>0){
+        int lastDigit = num % 10 ;
+        newNum = newNum*10 + lastDigit ;
+        num /= 10 ;
+    }
+    return origNum==newNum ;  
+}
 //main
 int main(){
-
+    int number;
+    cout<<"Enter number: ";
+    cin>>number;
+    bool res = isPalindrome(number);
+    if(res == 1) printf("%d is palindrome\n",number);
+    else printf("%d is not a palindrome\n",number);
     return 0 ;
 }
