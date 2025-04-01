7b88ddd079d1924f283fd2cbb2edbef632d8d608
ryan-4u <aaryanofficial64@gmail.com>
2025-04-01 18:48:10 +0530

    assign ques- largest of three numbers

--- C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/largestOfThree.cpp
+++ C++ with DSA/Basics of Programming/All Assignments/Function - Assignment/largestOfThree.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std ;
 //function
-
+int largestOfThree(int a ,int b , int c ){
+    if(a>c){
+        if(a>b) return a ;
+        else return b ;
+    }else{
+        if(c>b) return c ;
+        else return b ;
+    }
+}
 //main
 int main(){
-
+    int a , b , c ;
+    cout<<"Enter all three numbers: ";
+    cin>>a ;
+    cin>>b ;
+    cin>>c ;
+    cout<<"Largest of three numbers is: "<<largestOfThree(a,b,c)<<endl;
     return 0 ;
 }
