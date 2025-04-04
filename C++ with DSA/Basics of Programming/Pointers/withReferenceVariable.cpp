0b5ba5285f5c6505b7dab933196aec89a2848930
ryan-4u <aaryanofficial64@gmail.com>
2025-04-04 09:20:56 +0530

    call by reference with reference variables update

--- C++ with DSA/Basics of Programming/Pointers/withReferenceVariable.cpp
+++ C++ with DSA/Basics of Programming/Pointers/withReferenceVariable.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std ;
 
-void changeA(int &a){
-    a = 20 ;
-      cout<< a <<endl;
+void changeA(int &b){
+    b = 20 ;
+    cout<< b <<endl;
 }
 int main(){
     int a = 10 ;
