64ace0d25820fe7a4571d576effb803e08b20088
ryan-4u <aaryanofficial64@gmail.com>
2025-04-10 21:04:02 +0530

    insertion sort

---
+++ To DSA/DSA/Sorting/insertionSort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std ;
+
+void insertionSort( int *arr, int n ){
+
+    for( int i=1 ; i<n ; i++){
+        int curr = arr[i] ; // to soet first of unsorted element
+        int prev = i-1 ; // to run backward loop
+
+        while( prev>=0 && arr[prev] > curr){
+            swap( arr[prev] , arr[prev+1] ) ;
+            prev-- ;
+        }
+        // now prev = -1 -> if loop run completely
+        //        or a value less than the right posn to insert our curr
+        arr[prev + 1] = curr ;   
+    }
+    
+    cout<<"The sorted array : ";
+    for(int i=0 ; i<n ;i++){
+        cout<<arr[i]<<" " ;
+    }
+    cout<<endl ;
+
+    return ;
+}
+
+int main() {
+    int arr[] = { 5,4,1,3,2 } ;
+    int n = sizeof(arr) / sizeof(arr[0]) ;
+    insertionSort( arr,n ) ;
+}
