909b2d2b4ab180ac00832918117244c8a9de6024
ryan-4u <aaryanofficial64@gmail.com>
2025-07-28 15:50:31 +0530

    organising the files

--- To DSA/DSA/Bit Manipulation/compliment.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std ;
-int main(){
-    int a = 6 , b = 5 ;
-    cout<<" ~6 ="<< ( ~6 ) << endl ; // -7
-    cout<<" ~0 ="<< ( ~0 ) << endl ; // -1     
-    return 0 ;
-}
--- To DSA/DSA/Bit Manipulation/shiftOperators.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std ;
-int main(){
-     
-    cout<<" 7<<2 = "<< ( 7<<2 ) << endl ; // -7
-    cout<<" 7>>2 = "<< ( 7>>2 ) << endl ; // -1     
-    return 0 ;
-}
