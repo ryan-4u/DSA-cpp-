48e8e72d9b452a73a497cf1b9e2550499484c9e6
ryan-4u <aaryanofficial64@gmail.com>
2025-10-03 19:33:12 +0530

    resolved bubble sort / revision

--- DSA foundation/Sorting/bubbleSort.cpp
+++ DSA foundation/Sorting/bubbleSort.cpp
@@ -1,41 +1,46 @@
 #include<iostream>
-#include<aLgorithm>
-using namespace std ;
+#include<algorithm>
 
-// void swap( int *a , int *b) {
-//     int temp = *a ;
-//     *a = *b ;
-//     *b = temp ;
-//     return ;
-// }
+using namespace std;
 
-void bubbleSort( int *arr, int n ){
-
-    for( int outer=0 ; outer<n-1 ; outer++ ){
+// one thing to remember -> 
+// if no swap then already sorted : make code based on it
+void bubbleSort(int arr[],int n){
+    for(int x=0; x<n;x++){
         bool isSwap = false ;
-        for(int i=0 ; i<n-outer-1 ; i++){
-            if( arr[i] > arr[i+1] )
-            {
-                swap( arr[i],arr[i+1]) ;
-                isSwap = true ;
+        for(int i=1 ; i<n-x;i++){
+            if(arr[i]<arr[i-1]){
+                swap(arr[i],arr[i-1]);
+                isSwap = true
             }
         }
-        if( !isSwap ){
-        break ;
+        if( !isSwap){
+            break;
         }
     }
     
-    cout<<"The sorted array : ";
-    for(int i=0 ; i<n ;i++){
-        cout<<arr[i]<<" " ;
+    
+    cout<<"Elements after swapping: ";
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
     }
-    cout<<endl ;
 
-    return ;
 }
+int main(){
+    int n ;
+    cout<<"Enter no of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter your array elements:";
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Entered array elements are: ";
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    bubbleSort(arr,n);
 
-int main() {
-    int arr[] = { 5,4,1,3,2 } ;
-    int n = sizeof(arr) / sizeof(arr[0]) ;
-    bubbleSort( arr,n ) ;
+    return 0 ;
 }
