ad3bc0871264eb78fcd867f2a5c1e30f23f8cf34
ryan-4u <aaryanofficial64@gmail.com>
2025-10-05 15:46:31 +0530

    selection sort / revision /improvement in code

--- DSA foundation/Sorting/selectionSort.cpp
+++ DSA foundation/Sorting/selectionSort.cpp
@@ -1,29 +1,69 @@
-#include<iostream>
-using namespace std ;
+#include<iostream> 
+#include<algorithm>
+using namespace std;
 
-void selectionSort( int *arr, int n ){
+//OMG -> i improved the code
+void selectionSort(int arr[],int n){
 
-    for( int i=0 ; i<n-1 ; i++ ){
-        int minIdx = i ;
-        for( int j= i+1 ; j<n ;j++){
-            if( arr[j] < arr[minIdx] ){
-                minIdx = j ;
-            }
-        }
-        swap( arr[minIdx] , arr[i] ) ;
+    for(int i=0;i<n;i++){
+        int* minEle = min_element(arr+i,arr+n) ;
+        swap(arr[i],*minEle);
+    }    
+    cout<<"sorted array elements are: ";
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";    
     }
+    cout<<endl;
+}
+
+int main(){
+    int n ;
+    cout<<"Enter no of elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter your array elements:";
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Entered array elements are: ";
+    for(int i=0; i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    selectionSort(arr,n);
+
+    return 0 ;
+}
+
+
+
+
+// #include<iostream>
+// using namespace std ;
+
+// void selectionSort( int *arr, int n ){
+
+//     for( int i=0 ; i<n-1 ; i++ ){
+//         int minIdx = i ;
+//         for( int j= i+1 ; j<n ;j++){
+//             if( arr[j] < arr[minIdx] ){
+//                 minIdx = j ;
+//             }
+//         }
+//         swap( arr[minIdx] , arr[i] ) ;
+//     }
     
-    cout<<"The sorted array : ";
-    for(int i=0 ; i<n ;i++){
-        cout<<arr[i]<<" " ;
-    }
-    cout<<endl ;
+//     cout<<"The sorted array : ";
+//     for(int i=0 ; i<n ;i++){
+//         cout<<arr[i]<<" " ;
+//     }
+//     cout<<endl ;
 
-    return ;
-}
+//     return ;
+// }
 
-int main() {
-    int arr[] = { 5,4,1,3,2 } ;
-    int n = sizeof(arr) / sizeof(arr[0]) ;
-    selectionSort( arr,n ) ;
-}
+// int main() {
+//     int arr[] = { 5,4,1,3,2 } ;
+//     int n = sizeof(arr) / sizeof(arr[0]) ;
+//     selectionSort( arr,n ) ;
+// }
